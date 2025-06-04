644e4294390bca4b6520fafcd7d7b8dd717bb3b6
Ayush-DataAnalyst <ayushshimpi02@gmail.com>
2025-06-04 14:01:46 +0530

    setup added

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "ML Based Books Recommender System"
-AUTHOR_USER_NAME = "BOKTIAR AHMED BAPPY"
-SRC_REPO = "books_recommender"
+REPO_NAME = "Books-Recommendation-System"
+AUTHOR_USER_NAME = "Ayush-DataAnalyst"
+SRC_REPO = "books_recommendation_system"
 LIST_OF_REQUIREMENTS = []
 
 
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author="BOKTIAR AHMED BAPPY",
+    author="Ayush-DataAnalyst",
     description="A small local packages for ML based books recommendations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/entbappy/ML-Based-Book-Recommender-System",
-    author_email="entbappy73@gmail.com",
+    url="https://github.com/Ayush-DataAnalyst/Books-Recommendation-System",
+    author_email="ayushshimpi02@gmail.com",
     packages=find_packages(),
     license="MIT",
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     install_requires=LIST_OF_REQUIREMENTS
 )
